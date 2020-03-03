12026876dea62867b69e91dbad88e3c5b7fd4861
tonysoman0203 <somankit2000@gmail.com>
2020-03-03 15:12:10 +0800

    Updates

---
+++ precache-manifest.15cd1d306c938cc7a58c82ff2b5b7e09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adff0d2cfeaec58786e5c3ba005797cd",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "07e1ecbc1f0f27c111f7",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d8cbd2a844c2988b6e17",
+    "url": "/react-gh-pages/static/js/2.6abb62f7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-gh-pages/static/js/2.6abb62f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e1ecbc1f0f27c111f7",
+    "url": "/react-gh-pages/static/js/main.69284d22.chunk.js"
+  },
+  {
+    "revision": "e224faa1162b58826718",
+    "url": "/react-gh-pages/static/js/runtime-main.e42c533e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
